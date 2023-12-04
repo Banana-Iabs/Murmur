09aba963d9283ee3c81910b3f5d55201817a1709
Teo I <teoimototar@gmail.com>
2023-12-03 21:11:44 -0800

    Initial Commit

---
+++ Murmur/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Murmur
+//
+//  Created by Teo I on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Banana.Industries")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Murmur/MurmurApp.swift
@@ -0,0 +1,17 @@
+//
+//  MurmurApp.swift
+//  Murmur
+//
+//  Created by Teo I on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MurmurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
