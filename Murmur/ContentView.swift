83134e9104d7bdf8b1a9ae1403fb87c66d1ee5fa
Teo I <teoimototar@gmail.com>
2023-12-03 22:56:23 -0800

    added firebase integration to base app

--- Murmur/ContentView.swift
+++ Murmur/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Banana.Industries")
+            Text("Banana")
         }
         .padding()
     }
